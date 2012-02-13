07693a9ffe74435e27eb7189b6f21cbcb81f7432
Jan-Hendrik Hanne <git@janhanne.de>
2012-02-13 20:23:16 +0100

    adding copyright

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 /*
- * button drücken ruft inkweek aus , inkweek startet timer zum entprellen, leider stoppt der Timer nicht und lässt die lampen blinken, wenn clearflag in button interupt, dann kein start des timers.
- *
- */
+TriavaBuzzer - allows 4 Players to use a buzzer which activates a LED identifing the player who pressed its buzzer first.
+
+    Copyright (C) 2011,2012  Jan-Hendrik Hanne
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 
 #include  <msp430x20x2.h>
 #include <string.h>
